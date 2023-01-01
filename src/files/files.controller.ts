89e3995cc9679c1a33c367cbe0a6cd1f03e3443c
Alexander Mendivil <alexanderima@outlook.es>
2022-12-31 18:59:03 -0700

    Files res added and validations

---
+++ src/files/files.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException } from '@nestjs/common';
+import { UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+import { fileFilter } from './helpers/fileFilter';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter
+  }) )
+  uploadProductImage( @UploadedFile() file: Express.Multer.File ) {
+
+    if(!file){
+      throw new BadRequestException('Make sure that the file is an image')
+    }
+    return {fieldName: file.originalname};
+  }
+}
