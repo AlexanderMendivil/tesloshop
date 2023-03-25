c7ffcab79efeaa4a8c5c99455b5f36654ed994fc
Alexander Mendivil <alexanderima@outlook.es>
2023-03-24 19:55:46 -0700

    auth en otros modulos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,32 +3,37 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from 'src/auth/decorators';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-
+  
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
-
+  
   @Get()
   findAll(@Query() paginationDto:PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
-
+  
   @Get(':term')
   findOne(@Param('term' ) term: string) {
     return this.productsService.findOnePlain(term);
   }
-
+  
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(@Param('id', ParseUUIDPipe ) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
-
+  
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe ) id: string) {
     return this.productsService.remove(id);
   }
