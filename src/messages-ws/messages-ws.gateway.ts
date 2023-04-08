5760ac8f639258a833d8ed3a43b51b6b186c0838
Alexander Mendivil <alexanderima@outlook.es>
2023-04-08 15:12:06 -0700

    get connected users

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,17 +1,20 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
+  @WebSocketServer() wss: Server;
   constructor(private readonly messagesWsService: MessagesWsService) {}
   
-  handleConnection(client: Socket, ...args: any[]) {
+  handleConnection(client: Socket ) {
     console.log('client connected', client.id)
+    this.messagesWsService.registerClient(client);
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
   }
   
   handleDisconnect(client: Socket) {
-    console.log('client disconnecter', client.id)
+    this.messagesWsService.removeClient(client.id);
   }
 }
