00c4a9ba2e317c494507c24b6080fc125214c031
Alexander Mendivil <alexanderima@outlook.es>
2023-04-08 14:35:12 -0700

    socket config

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,17 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
+  
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('client connected', client.id)
+  }
+  
+  handleDisconnect(client: Socket) {
+    console.log('client disconnecter', client.id)
+  }
 }
