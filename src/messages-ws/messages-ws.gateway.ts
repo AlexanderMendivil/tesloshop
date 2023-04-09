89c3c4e4dac2034fb0e3aaccfa524db0355b0ec4
Alexander Mendivil <alexanderima@outlook.es>
2023-04-08 18:16:25 -0700

    broadcast messages

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,6 +1,7 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from 'src/products/dto/new-message-dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -16,5 +17,11 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
+  }
+  
+  @SubscribeMessage('message-from-client')
+  onMessageFromClient( client: Socket, payload: NewMessageDto ){
+    // client.broadcast.emit('messages-from-server', {fullName: 'test', message: payload.message || 'no message'})
+    this.wss.emit('messages-from-server', {fullName: 'test', message: payload.message || 'no message'})
   }
 }
